22f825a92390e65717de07e2625d90374708d6a3
VichetN <vichet.noeng66@gmail.com>
2024-02-24 14:20:24 +0700

    update

--- src/configs/siteConfig.js
+++ src/configs/siteConfig.js
@@ -3,7 +3,7 @@ import { getDomain } from "@/lib/dev";
 export const siteConfig = {
   name: "VichetN",
   url: getDomain(),
-  ogImage: "/image/1.jpg",
+  ogImage: "/images/1.jpg",
   author: "VichetN",
   description: "Siem Reap Temples images, parralax animation",
 };
